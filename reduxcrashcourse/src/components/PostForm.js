e1d474ea4912ee1c4a0a95e9683d3275e1894d6a
pratikkadam22 <pratikkadam2212@gmail.com>
2020-07-02 20:12:20 -0700

    new post can be added using redux

--- reduxcrashcourse/src/components/PostForm.js
+++ reduxcrashcourse/src/components/PostForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
 class PostForm extends Component {
     constructor(props) {
@@ -21,17 +24,8 @@ class PostForm extends Component {
         const post = {
             title: this.state.title,
             body: this.state.body
-        }
-
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-        .then(res => res.json())
-        .then(data => console.log(data));
+        };
+        this.props.createPost(post)
     }
 
     render() {
@@ -54,4 +48,8 @@ class PostForm extends Component {
     }
 }
 
-export default PostForm;
+PostForm.propTypes = {
+    createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, { createPost })(PostForm);
